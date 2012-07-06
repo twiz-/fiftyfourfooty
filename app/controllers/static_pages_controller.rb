626070349ec293943f8d42330315181e83249c02
Tony Ramirez <tony.michaelramirez@gmail.com>
2012-07-06 15:26:31 -0700

    Add a static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Fiftyfourfooty::Application.routes.draw do
+  
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/contact"
+  get "static_pages/about"
+  get "static_pages/blog"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

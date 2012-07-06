b8da30133cf4c699379ddb62a44880df5d3c90a0
Tony Ramirez <tony.michaelramirez@gmail.com>
2012-07-06 15:15:36 -0700

    Set up app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiftyfourfooty::Application.config.secret_token = 'bc7f973bcf59a4be826781b2f8ffdaa44683b51f2deaf52745f225a188bcb2c6f8f3a67c93b2d5e9383f77e7252e2faf3c83d6ef32b32e764ae123feacdb6415'

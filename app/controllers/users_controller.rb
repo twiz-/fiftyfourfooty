170b7e701cae93c139a0be70db249b7a6c6713e8
Tony Ramirez <tony.michaelramirez@gmail.com>
2012-07-22 10:43:01 -0700

    Finish edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,40 @@
 class UsersController < ApplicationController
-  
-  def show
-    @user = User.find(params[:id])
-  end
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy] #this makes is a filter that authenticates only a signed in user can edit, see the user index or update their own profile. :)
+  before_filter :correct_user,   only: [:edit, :update] # this adds another filter that the right signed in user edits their own data and applies it to 
+  before_filter :admin_user,     only: :destroy
+   
   
   def new
-    @user = User.new
+    @user = User.new #this creates a new instance of a user form the user class, useful to show a users info in a view
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy #this finds the user you want to delete and destroy deletes it
+    flash[:success] = "User destroyed, ouch.." # this lets you know the action was completed
+    redirect_to users_path #This sends you back to all the users at the end of deletion
+  end
+  
+  def index 
+    #Requires signed in user to see all the users
+    #@users = User.all is an instance of the user class so we can use it in the view, each method in the controller is a view and the code within it represents what we want that page to do, the model defines what it 'can' do and the view shows it. :)
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def show
+    @user = User.find(params[:id]) #This retrieves a user from the db by it's id and shows the user info for profile information 
+  end
+  
+  def edit #@user = User.find(params[:id]) is not needed because of the correct_user filter same with update(1st line taken away)
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success]= "Profile updated, nicely done"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -18,4 +47,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  private 
+  
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+      
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_path, notice: "Sign in first." #This I believe, gets called when there is an error, and redirects user to the sign in page saying sign in first
+    end 
+    
+    def correct_user
+      @user = User.find(params[:id]) #this instantiates a user to find it, then redirects the user to home unless its the current user. Replace code in edit and update.
+      redirect_to(root_path) unless current_user?(@user) #uses currentuser? defined in helpers/sessions_helper.rb
+    end
+  end  
 end
